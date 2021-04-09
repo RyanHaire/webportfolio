80d05dfd2297ff2b8b0bc93840dadc5004954506
Ryan Haire <rwhaire@hotmail.com>
2021-04-09 03:36:41 -0400

    changed browserrouter to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import {HashRouter as Router, Switch, Route } from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen'
 import PortfolioScreen from './screens/PortfolioScreen'
 import AboutScreen from './screens/AboutScreen'
